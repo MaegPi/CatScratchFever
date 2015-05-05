af3d9c85a8bcdc9412d58d994f6886e85d0dfee7
MaegPi <jeska.vandetta@gmail.com>
2015-05-05 07:39:30 +0100

    WIP
    Cat Scratch Fever game (WIP)

---
+++ CatScratchFever/FormView.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CatScratchFever
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormView));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tmrMoving = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(39, 431);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 132);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tmrMoving
+            // 
+            this.tmrMoving.Enabled = true;
+            this.tmrMoving.Interval = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(704, 592);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(204)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(704, 592);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "FormView";
+            this.Text = "Cat Scratch Fever!";
+            this.Load += new System.EventHandler(this.FormView_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormView_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FormView_KeyDown);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.FormView_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer tmrMoving;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CatScratchFever/FormView.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CatScratchFever
+{
+    public partial class FormView : Form
+    {
+        bool right, left;
+        bool jump;
+        int G = 30;
+        int Force;
+
+     /*  enum Position
+        {
+            Left, Right, Up, Down
+        }
+        */
+        private int _x;
+        private int _y;
+       
+       
+        public FormView()
+        {
+            InitializeComponent();
+
+            _x = 50;
+            _y = 50;
+            
+         }
+
+        private void FormView_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        { }
+
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+
+            e.Graphics.FillRectangle(Brushes.BlueViolet, _x, _y, 100, 100);
+            //e.Graphics.DrawImage(new Bitmap("yarn.png"), _x, _y, 32, 56);
+        }
+
+
+        
+        private void FormView_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Right
+            if (e.KeyCode == Keys.Right)
+            {
+                pictureBox1.Left += 5;
+            }
+            if (pictureBox1.Right >= panel1.Width)
+            {
+                pictureBox1.Left = panel1.Width - pictureBox1.Width;
+                }
+
+            //Left
+            if (e.KeyCode == Keys.Left)
+            {
+                pictureBox1.Left -= 5;
+            }
+           if (pictureBox1.Left <= 0)
+                {
+                    pictureBox1.Left = 0;
+                }
+           if (pictureBox1.Left <= 0)
+           {
+               pictureBox1.Left = 0;
+            }
+
+            //Down
+            if (e.KeyCode == Keys.Down)
+            {
+                pictureBox1.Top += 5;
+            }
+           if (pictureBox1.Top + pictureBox1.Height >= panel1.Height)
+           {
+                 pictureBox1.Top = panel1.Height - pictureBox1.Height;
+              }
+            
+            //Up
+            if (e.KeyCode == Keys.Up)
+            {
+                pictureBox1.Top -= 5;
+            }
+            if (pictureBox1.Top <= 0)
+            {
+                pictureBox1.Top = 0;
+            }
+          
+
+
+
+            //Diagonal\\
+
+            //UpRight
+            //Up
+            if (e.KeyCode == (Keys.Up & Keys.Right))
+            {
+                ///left = true;
+          
+                pictureBox1.Left += 5;
+                pictureBox1.Top -= 5;
+            }
+            
+            //UpLeft
+            if (e.KeyCode == (Keys.Up & Keys.Left))
+            {
+                ///left = true;
+                
+                pictureBox1.Left -= 5;
+                pictureBox1.Top -= 5;
+            }
+
+            //DownRight
+            if (e.KeyCode == (Keys.Down & Keys.Right))
+            {
+                //left = true;
+                pictureBox1.Top += 5;
+                pictureBox1.Left += 5;
+            }
+
+            //DownLeft
+            if (e.KeyCode == (Keys.Down | Keys.Left))
+            {
+                //left = true;
+                pictureBox1.Top += 5;
+                pictureBox1.Left -= 5;
+            }
+
+
+            //Jump
+            if (e.KeyCode == Keys.Space)
+                {
+                    //jump = true;
+                    Force = G;
+                    pictureBox1.Top -= Force;
+                    Force -= 1;
+                }
+            
+            
+            //Quit
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+
+      
+        }
+       
+        
+        private void tmrMoving_Tick(object sender, EventArgs e)
+        {
+            /*if (right==true)
+            {
+                pictureBox1.Left += 5;
+            }
+            if (left == true)
+            {
+                pictureBox1.Left -= 5;
+            }
+            if (jump == true) { 
+                //falling if players has jumped already
+                pictureBox1.Top -= Force;
+                Force -= 1;
+            }
+            if (pictureBox1.Top + pictureBox1.Height >= panel1.Height)
+            {
+                pictureBox1.Top = panel1.Height - pictureBox1.Height;
+                jump = false;
+            }
+            else {
+                pictureBox1.Top += 5; //Falling
+            }*/
+
+           /* if (_objPosition == Position.Right) {
+                _x += 10;
+            }
+            if (_objPosition == Position.Left) {
+                _x -= 10;
+            }
+            if (_objPosition == Position.Up)
+            {
+                _y -= 10;
+            }
+            if (_objPosition == Position.Down)
+            {
+                _y += 10;
+            }*/
+            _x += 10;
+            Invalidate();
+            
+        }
+
+
+     /*  //pictureBox1.Location = new Point(_xCat, _yCat);
+        }
+        */
+       
+        /*Change colour of yarn?
+         * 
+         * Color color = Color.Black; //Your desired colour
+
+byte r = color.R; //For Red colour
+
+Bitmap bmp = new Bitmap(this.BackgroundImage);
+for (int x = 0; x < bmp.Width; x++)
+{
+    for (int y = 0; y < bmp.Height; y++)
+    {
+        Color gotColor = bmp.GetPixel(x, y);
+        gotColor = Color.FromArgb(r, gotColor.G, gotColor.B);
+        bmp.SetPixel(x, y, gotColor);
+    }
+}
+         * */
+
+        private void FormView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
